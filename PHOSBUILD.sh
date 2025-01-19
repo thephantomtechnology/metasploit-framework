904ce6116b1309b823799b32cc88c468bc081bbe
The Phantom Technology -Advanced Pentest <155259033+thephantomtechnology@users.noreply.github.com>
2025-01-19 19:36:08 +0800

    更新 PHOSBUILD.sh

--- PHOSBUILD.sh
+++ PHOSBUILD.sh
@@ -1,5 +1,10 @@
 bundle install
 
+rm -rf /opt/PHANTX/metasploit-framework/*
+
+rm -rf /opt/ANDRAX/metasploit-framework
+
+
 cp -Rf $(pwd) /opt/PHANTX/metasploit-framework
 
 cp -Rf phantxbin/* /opt/PHANTX/bin
