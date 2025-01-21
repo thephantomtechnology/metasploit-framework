bf1c0772ac969d62fd91cd9b57fcbdfe5cdc71af
The Phantom Technology -Advanced Pentest <155259033+thephantomtechnology@users.noreply.github.com>
2025-01-21 14:54:47 +0800

    更新 PHOSBUILD.sh

--- PHOSBUILD.sh
+++ PHOSBUILD.sh
@@ -1,13 +1,32 @@
 bundle install
 
-rm -rf /opt/PHANTX/metasploit-framework/*
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Bundle install... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
 
-rm -rf /opt/ANDRAX/metasploit-framework
+bundle update --bundler
+
+rm -rf /opt/PHANTX/metasploit-framework
 
 rm -rf .git*
 
 cp -Rf $(pwd) /opt/PHANTX/metasploit-framework
 
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
 cp -Rf phantxbin/* /opt/PHANTX/bin
 
-chmod -R 755 /opt/PHANTX/bin
+chown -R phantx:phantx /opt/PHANTX/
+chmod -R 755 /opt/PHANTX/
